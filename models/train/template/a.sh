a1d1f768f19ac45e22abc4b93f9f4373ce64ff90
Gaetan Manchon <gmanchon@gmail.com>
2020-03-08 15:10:46 +0100

    configuration variables moved from conf.py to setup.py in order to allow setup.py to use them (import did not work)

--- models/train/template/a.sh
+++
@@ -1,3 +0,0 @@
-# this was a test, do not use
-source .conf.py
-echo $PROJECT_ID
--- models/train/template/setup.py
+++ models/train/template/setup.py
@@ -1,14 +1,68 @@
 from setuptools import find_packages
 from setuptools import setup
 
+#   .-.-.   .-.-.   .-.-.   .-.-.   .-.-.   .-.-.   .-.-.   .-.-
+#  / / \ \ / / \ \ / / \ \ / / \ \ / / \ \ / / \ \ / / \ \ / / \
+# `-'   `-`-'   `-`-'   `-`-'   `-`-'   `-`-'   `-`-'   `-`-'
+#
+# this file is used by gcp and also provides parameters to Makefile
+#
+
+# ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^
+#
+# python model package
+#
+
+# folder name
+PACKAGE_NAME="StaticModel"
+PACKAGE_DESCRIPTION="static prediction model"
+
+# file name
+FILENAME="trainer"
+
+# ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^
+#
+# gcp project
+#
+
+# project id
+PROJECT_ID="le-wagon-data-grupo-bimbo"
+
+# ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^
+#
+# gcp ai platform
+#
+
+# model training conf
+REGION="europe-west1"
+PYTHON_VERSION=3.7
+RUNTIME_VERSION=1.15
+FRAMEWORK="scikit-learn"
+
 REQUIRED_PACKAGES = [
     'gcsfs==0.6.0',
     'google-cloud-storage==1.26.0',
     'pandas==0.24.2',
     'scikit-learn==0.20.4']
 
-PACKAGE_NAME='StaticModel'                        # model folder name
-PACKAGE_DESCRIPTION='static prediction model'     # model folder name
+# ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^
+#
+# gcp storage
+#
+
+# model training bucket
+BUCKET_NAME="wagon-data-grupo-bimbo-sales"
+
+# model training folder
+JOB_FOLDER="trainings"
+
+# job prefix
+JOB_PREFIX="static_model"
+
+# ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^ ~ ^
+#
+# ai platform setup
+#
 
 setup(name=PACKAGE_NAME,
     version='1.0',
